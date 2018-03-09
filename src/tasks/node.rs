2d5bda6ba0615b3fb28d4f38a93c7f7cdca6b7c5
Ron Waldon <jokeyrhyme@gmail.com>
2018-03-10 08:55:52 +1100

    pkg: node: update npm on Windows

--- src/tasks/node.rs
+++ src/tasks/node.rs
@@ -1,5 +1,7 @@
 use utils;
 
+const ERROR_MSG: &str = "error: node";
+
 pub fn sync() {
     if !utils::node::has_npm() {
         return;
@@ -9,5 +11,15 @@ pub fn sync() {
 pub fn update() {
     if !utils::node::has_npm() {
         return;
+    }
+
+    if utils::node::has_npx() {
+        // https://www.npmjs.com/package/npm-windows-upgrade
+        #[cfg(windows)]
+        utils::process::command_spawn_wait(
+            "npx",
+            &["-q", "npm-windows-upgrade", "--npm-version", "latest"],
+        )
+            .expect(ERROR_MSG);
     }
 }
