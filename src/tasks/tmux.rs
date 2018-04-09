8b6e5c4e7693cf77218e873ab8d792090c5b7ded
Ron Waldon <jokeyrhyme@gmail.com>
2018-04-10 06:04:00 +1000

    pkg: tmux: install/clean/update tpm plugins

--- src/tasks/tmux.rs
+++ src/tasks/tmux.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 
 use utils;
 
+const ERROR_MSG: &str = "error: tmux";
+
 pub fn sync() {
     if !has_tmux() {
         return;
@@ -13,12 +15,48 @@ pub fn sync() {
     let dest = utils::env::home_dir().join(Path::new(".tmux.conf"));
 
     utils::fs::symbolic_link_if_exists(&src, &dest);
+
+    let tpm_path = utils::env::home_dir().join(Path::new(".tmux/plugins/tpm"));
+    if utils::git::path_is_git_repository(&tpm_path) {
+        // TODO: install tpm plugin for tmux when it is absent
+
+        let empty_args: &[&str] = &[];
+
+        let tpm_install_path = tpm_path.join(Path::new("bin/install_plugins"));
+        utils::process::command_spawn_wait(
+            tpm_install_path.into_os_string().to_str().unwrap(),
+            &empty_args,
+        ).expect(ERROR_MSG);
+
+        let tpm_clean_path = tpm_path.join(Path::new("bin/clean_plugins"));
+        utils::process::command_spawn_wait(
+            tpm_clean_path.into_os_string().to_str().unwrap(),
+            &empty_args,
+        ).expect(ERROR_MSG);
+    }
 }
 
-pub fn update() {}
+pub fn update() {
+    if !has_tmux() {
+        return;
+    }
+
+    println!("pkg: tmux: updating ...");
+
+    let tpm_path = utils::env::home_dir().join(Path::new(".tmux/plugins/tpm"));
+    if utils::git::path_is_git_repository(&tpm_path) {
+        utils::git::pull(&tpm_path);
+
+        let tpm_update_path = tpm_path.join(Path::new("bin/update_plugins"));
+        utils::process::command_spawn_wait(
+            tpm_update_path.into_os_string().to_str().unwrap(),
+            &["all"],
+        ).expect(ERROR_MSG);
+    }
+}
 
 fn has_tmux() -> bool {
-    match utils::process::command_output("tmux", &["--version"]) {
+    match utils::process::command_output("tmux", &["-V"]) {
         Ok(output) => {
             return output.status.success();
         }
