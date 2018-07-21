42837aa1973c22b4b2c854796fbe0fbd1ff911f1
Ron Waldon <jokeyrhyme@gmail.com>
2018-07-21 05:13:41 +0000

    shfmt: use GHRTask struct to de-dupe logic

--- src/tasks/shfmt.rs
+++ src/tasks/shfmt.rs
@@ -1,82 +1,40 @@
-use std;
+use regex::Regex;
 
-use utils;
-use utils::github::{Asset, Release};
+use lib::ghrtask::GHRTask;
+use utils::github::Asset;
 use utils::golang::{arch, os};
 
 pub fn sync() {
-    println!("shfmt: syncing ...");
-
-    if !is_installed() {
-        let release = match utils::github::latest_release("mvdan", "sh") {
-            Ok(r) => r,
-            Err(error) => {
-                println!("error: shfmt: {}", error);
-                return;
-            }
-        };
-        install_release_asset(release);
+    match GHR_TASK.sync() {
+        Ok(_) => {}
+        Err(_) => {}
     }
 }
 
 pub fn update() {
-    if !is_installed() {
-        return;
+    match GHR_TASK.update() {
+        Ok(_) => {}
+        Err(_) => {}
     }
-
-    println!("shfmt: updating ...");
-
-    match utils::process::command_output("shfmt", &["--version"]) {
-        Ok(output) => {
-            let stdout = std::str::from_utf8(&output.stdout).unwrap_or_default();
-
-            match utils::github::release_versus_current(stdout, "mvdan", "sh") {
-                Some(r) => install_release_asset(r),
-                None => {}
-            }
-        }
-        Err(_error) => {}
-    };
 }
 
-fn install_release_asset(release: Release) {
-    let asset = match latest_asset(&release) {
-        Some(a) => a,
-        None => {
-            println!("shfmt: no asset matches OS and ARCH");
-            return;
-        }
-    };
-
-    println!("shfmt: installing ...");
-
+const GHR_TASK: GHRTask = GHRTask {
+    asset_filter: asset_filter,
     #[cfg(windows)]
-    let bin_path = utils::env::home_dir().join(".local/bin/shfmt.exe");
+    command: "shfmt.exe",
     #[cfg(not(windows))]
-    let bin_path = utils::env::home_dir().join(".local/bin/shfmt");
+    command: "shfmt",
+    repo: ("mvdan", "sh"),
+    trim_version: trim_version,
+    version_arg: "--version",
+};
 
-    utils::github::download_release_asset(asset, &bin_path);
+fn asset_filter(asset: &Asset) -> bool {
+    let re = Regex::new(&format!("^shfmt_.*_{}_{}(\\.exe)?$", os(), arch())).unwrap();
+
+    re.is_match(&asset.name)
 }
 
-fn is_installed() -> bool {
-    match utils::process::command_output("shfmt", &["--version"]) {
-        Ok(output) => output.status.success(),
-        Err(_error) => false,
-    }
-}
-
-fn latest_asset(release: &Release) -> Option<Asset> {
-    return release
-        .assets
-        .to_vec()
-        .into_iter()
-        .filter(|asset| {
-            #[cfg(windows)]
-            let name = format!("shfmt_{}_{}_{}.exe", release.tag_name, os(), arch());
-            #[cfg(not(windows))]
-            let name = format!("shfmt_{}_{}_{}", release.tag_name, os(), arch());
-
-            asset.name == name
-        })
-        .next();
+fn trim_version(stdout: String) -> String {
+    String::from(stdout.trim())
 }
