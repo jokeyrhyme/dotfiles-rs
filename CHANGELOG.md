ad7c6dba7d9ffe9750930aa56e9e43dfd7697cf7
Ron Waldon <jokeyrhyme@gmail.com>
2018-11-18 15:49:57 +1100

    goget: no longer run `gometalinter -install`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Changed
+
+- goget: no longer run `gometalinter --install`
+
 ## [0.22.2] - 2018-11-13
 
 ### Changed
--- src/tasks/goget.rs
+++ src/tasks/goget.rs
@@ -89,12 +89,6 @@ fn sync() -> task::Result {
         println!("warning: goget: unable to install packages: {}", error);
     };
 
-    if which::which("gometalinter").is_ok() {
-        if let Err(error) = utils::process::command_spawn_wait("gometalinter", &["--install"]) {
-            println!("warning: goget: unable to install linters: {}", error);
-        };
-    };
-
     Ok(Status::Done)
 }
 
@@ -110,14 +104,6 @@ fn update() -> task::Result {
 
     if let Err(error) = utils::process::command_spawn_wait("go", &install_args) {
         println!("warning: goget: unable to update packages: {}", error);
-    };
-
-    if which::which("gometalinter").is_ok() {
-        if let Err(error) =
-            utils::process::command_spawn_wait("gometalinter", &["--install", "--force"])
-        {
-            println!("warning: goget: unable to update linters: {}", error);
-        };
     };
 
     Ok(Status::Changed(
