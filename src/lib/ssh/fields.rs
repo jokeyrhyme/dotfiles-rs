e212973f03e5edb111496a9b3c5656bc5491e5c3
Ron Waldon <jokeyrhyme@gmail.com>
2018-12-17 11:42:22 +0000

    ssh: define constants and the `Field` enum

--- src/lib/ssh/fields.rs
+++ src/lib/ssh/fields.rs
@@ -1,15 +1,52 @@
 use std::{
     fmt::{self, Display},
     i32,
+    path::PathBuf,
 };
 
 use regex;
 
+const ANY: &str = "any";
 const ASK: &str = "ask";
 const AUTO: &str = "auto";
 const AUTOASK: &str = "autoask";
+const CONFIRM: &str = "confirm";
+const INET: &str = "inet";
+const INET6: &str = "inet6";
+const MD5: &str = "md5";
 const NO: &str = "no";
+const SHA256: &str = "sha256";
 const YES: &str = "yes";
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum AnyInetInet6 {
+    Any,
+    Inet,
+    Inet6,
+}
+
+impl Display for AnyInetInet6 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            AnyInetInet6::Any => write!(f, "{}", ANY),
+            AnyInetInet6::Inet => write!(f, "{}", INET),
+            AnyInetInet6::Inet6 => write!(f, "{}", INET6),
+        }
+    }
+}
+
+impl<S> From<S> for AnyInetInet6
+where
+    S: Into<String> + AsRef<str>,
+{
+    fn from(source: S) -> Self {
+        match source.as_ref() {
+            INET => AnyInetInet6::Inet,
+            INET6 => AnyInetInet6::Inet6,
+            _ => AnyInetInet6::Any,
+        }
+    }
+}
 
 // Duration is for "TIME FORMATS" fields, see `man sshd_config(5)`
 #[derive(Clone, Debug, PartialEq)]
@@ -42,6 +79,125 @@ where
                     Duration::Time(String::new())
                 }
             }
+        }
+    }
+}
+
+pub enum Field {
+    AddKeysToAgent(YesNoAskConfirm),
+    AddressFamily(AnyInetInet6),
+    BatchMode(YesNo),
+    BindAddress(YesNo),
+    BindInterface(YesNo),
+    CanonicalDomains(YesNo),
+    CanonicalizeFallbackLocal(YesNo),
+    CanonicalizeHostname(YesNo),
+    CanonicalizeMaxDots(YesNo),
+    CanonicalisePermittedCNAMEs(String),
+    CertificateFile(PathBuf),
+    ChallengeResponseAuthentication(YesNo),
+    CheckHostIP(YesNo),
+    Ciphers(Vec<String>),
+    ClearAllForwardings(YesNo),
+    Compression(YesNo),
+    ConnectionAttempts(i32),
+    ConnectTimeout(i32),
+    ControlMaster(YesNoAskAutoAutoAsk),
+    ControlPath(PathBuf),
+    ControlPersist(YesNoDuration),
+    DynamicForward(String),
+    EnableSSHKeysing(YesNo),
+    EscapeChar(String), // character
+    ExitOnForwardFailure(YesNo),
+    FingerprintHash(Md5Sha256),
+    ForwardAgent(YesNo),
+    ForwardX11(YesNo),
+    ForwardX11Timeout(Duration),
+    ForwardX11Trusted(YesNo),
+    GatewayPorts(YesNo),
+    GlobalKnownHostsFile(PathBuf),
+    GSSAPIAuthentication(YesNo),
+    GSSAPIDelegateCredentials(YesNo),
+    HashKnownHosts(YesNo),
+    HostbasedAuthentication(YesNo),
+    HostbasedKeyTypes(Vec<String>),
+    HostKeyAlgorithms(Vec<String>),
+    HostKeyAlias(String),
+    Hostname(String),
+    IdentitiesOnly(YesNo),
+    IdentityAgent(PathBuf),
+    IdentityFile(PathBuf),
+    IgnoreUnknown(String),
+    Include(Vec<PathBuf>),
+    IPQoS(String),
+    KbdInteractiveAuthentication(YesNo),
+    KbdInteractiveDevices(Vec<String>),
+    KexAlgorithms(Vec<String>),
+    LocalCommand(String),
+    LocalForward(String),
+    LogLevel(String),
+    MACs(Vec<String>),
+    NoHostAuthenticationForLocalhost(YesNo),
+    NumberOfPasswordPrompts(i32),
+    PasswordAuthentication(YesNo),
+    PermitLocalCommand(YesNo),
+    PKCS11Provider(String),
+    Port(i32),
+    PreferredAuthentications(Vec<String>),
+    ProxyCommand(String),
+    ProxyJump(String),
+    ProxyUseFdpass(YesNo),
+    PubkeyAcceptedKeyTypes(Vec<String>),
+    PubkeyAuthentication(YesNo),
+    RekeyLimit(String), // fmt: size [duration]
+    RemoteCommand(String),
+    RemoteForward(String),
+    RequestTTY(String), // enum: no, yes, force, auto
+    RevokedHostKeys(PathBuf),
+    RhostsRSAAuthentication(YesNo),
+    RSAAuthentication(YesNo),
+    SendEnv(String),
+    ServerAliveCountMax(i32),
+    ServerAliveInterval(i32),
+    StreamLocalBindMask(YesNo),
+    StreamLocalBindUnlink(YesNo),
+    StrictHostKeyChecking(YesNoAsk),
+    SyslogFacility(String),
+    TCPKeepAlive(YesNo),
+    Tunnel(String), // enum: yes, point-to-point, ethernet, no
+    TunnelDevice(String),
+    UpdateHostKeys(YesNoAsk),
+    UsePrivilegedPort(YesNo),
+    User(String),
+    UserKnownHostsFile(PathBuf),
+    VerifyHostKeyDNS(YesNoAsk),
+    VisualHostKey(YesNo),
+    XAuthLocation(PathBuf),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Md5Sha256 {
+    Md5,
+    Sha256,
+}
+
+impl Display for Md5Sha256 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Md5Sha256::Md5 => write!(f, "{}", MD5),
+            Md5Sha256::Sha256 => write!(f, "{}", SHA256),
+        }
+    }
+}
+
+impl<S> From<S> for Md5Sha256
+where
+    S: Into<String> + AsRef<str>,
+{
+    fn from(source: S) -> Self {
+        match source.as_ref() {
+            MD5 => Md5Sha256::Md5,
+            _ => Md5Sha256::Sha256,
         }
     }
 }
@@ -142,6 +298,42 @@ impl<'a> From<&'a YesNoAskAutoAutoAsk> for String {
             YesNoAskAutoAutoAsk::YesNoAsk(ynaaa) => String::from(ynaaa),
             YesNoAskAutoAutoAsk::Auto => String::from(AUTO),
             YesNoAskAutoAutoAsk::AutoAsk => String::from(AUTOASK),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum YesNoAskConfirm {
+    YesNoAsk(YesNoAsk),
+    Confirm,
+}
+
+impl Display for YesNoAskConfirm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            YesNoAskConfirm::YesNoAsk(ynac) => write!(f, "{}", ynac),
+            YesNoAskConfirm::Confirm => write!(f, "{}", CONFIRM),
+        }
+    }
+}
+
+impl<S> From<S> for YesNoAskConfirm
+where
+    S: Into<String> + AsRef<str>,
+{
+    fn from(source: S) -> Self {
+        match source.as_ref() {
+            YES | NO | ASK => YesNoAskConfirm::YesNoAsk(YesNoAsk::from(source)),
+            _ => YesNoAskConfirm::Confirm,
+        }
+    }
+}
+
+impl<'a> From<&'a YesNoAskConfirm> for String {
+    fn from(source: &YesNoAskConfirm) -> String {
+        match source {
+            YesNoAskConfirm::YesNoAsk(ynaaa) => String::from(ynaaa),
+            YesNoAskConfirm::Confirm => String::from(CONFIRM),
         }
     }
 }
